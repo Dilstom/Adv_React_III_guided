85269f9c519d5617f2de79479889d1eaa12d810e
Dil Sombayeva <dilyasom@gmail.com>
2019-09-13 12:23:47 -0700

    create react app

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DynamicTitle from './DynamicTitle';
+import './App.css';
+
+function App() {
+ return (
+  <div className="App">
+   <DynamicTitle />
+  </div>
+ );
+}
+
+export default App;
