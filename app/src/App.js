b5eabece8ee881d662e433bfd91a1ba4cf227deb
Dil Sombayeva <dilyasom@gmail.com>
2019-09-13 15:47:58 -0700

    custom hook

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,85 @@
-import React from 'react';
-import DynamicTitle from './DynamicTitle';
+import React, { useState } from 'react';
+// import DynamicTitle from './DynamicTitle';
 import './App.css';
 
 function App() {
+ const [password, setPassword] = useState('');
+ const [isValid, setIsValid] = useState(false);
+ //  const passValidatorArray = usePasswordValidator();
+ const validatePass = pass => {
+  if (pass.length > 7) {
+   setIsValid(true);
+  } else {
+   setIsValid(false);
+  }
+ };
+ //  console.log(passValidatorArray);
+ console.log(isValid, setIsValid);
  return (
   <div className="App">
-   <DynamicTitle />
+   {/* <DynamicTitle /> */}
+   <p>Make sure your password is at least 8 chars</p>
+   <form action="">
+    <label>Create a New Password</label>
+    <input
+     placeholder="Create Password"
+     value={password}
+     onChange={e => {
+      setPassword(e.target.value);
+      validatePass(e.target.value);
+     }}
+    />
+    {isValid ? <h3>Password is Valid</h3> : <h3>Password is Invalid</h3>}
+   </form>
   </div>
  );
 }
 
 export default App;
+
+// const usePasswordValidator = () => {
+//  const [isValid, setIsValid] = useState(false);
+
+//  const validatePass = pass => {
+//   if (pass.length > 7) {
+//    setIsValid(true);
+//   } else {
+//    setIsValid(false);
+//   }
+//  };
+//  return [isValid, validatePass, 'Jow'];
+//  //  same as
+//  //  return {
+//  //      isValid: isValid,
+//  //      validatePass: validatePass
+//  //  }
+// };
+
+// function App() {
+//  const [password, setPassword] = useState('');
+//  const [isValid, validatePass, person] = usePasswordValidator();
+//  //  const passValidatorArray = usePasswordValidator();
+
+//  //  console.log(passValidatorArray);
+//  console.log(isValid, validatePass, person);
+//  return (
+//   <div className="App">
+//    {/* <DynamicTitle /> */}
+//    <p>Make sure your password is at least 8 chars</p>
+//    <form action="">
+//     <label>Create a New Password</label>
+//     <input
+//      placeholder="Create Password"
+//      value={password}
+//      onChange={e => {
+//       setPassword(e.target.value);
+//       validatePass(e.target.value);
+//      }}
+//     />
+//     {isValid ? <h3>Password is Valid</h3> : <h3>Password is Invalid</h3>}
+//    </form>
+//   </div>
+//  );
+// }
+
+// export default App;
--- app/src/DynamicTitle.js
+++ app/src/DynamicTitle.js
@@ -1,40 +1,48 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+
+const useCustomHook = initialValue => {
+ const [inputText, setInputText] = useState(initialValue);
+ const addSomething = e => {
+  setInputText(e.target.value + e.target.value);
+ };
+ return [inputText, addSomething];
+};
 
 const DynamicTitle = () => {
-  const [title, setTitle] = useState("Hooks are so fun!");
-  const [inputText, setInputText] = useState("");
+ const [title, setTitle] = useState('This is a class component');
+ const [inputText, setInputText] = useCustomHook('');
 
-  const handleChanges = e => {
-    setInputText(e.target.value);
-  };
+ const handleChanges = e => {
+  setInputText(e);
+ };
 
-  const changeTitle = e => {
-    e.preventDefault();
-    setTitle(inputText);
-    setInputText("");
-  };
+ const changeTitle = e => {
+  e.preventDefault();
+  setTitle(inputText);
+  setInputText('');
+ };
 
-  return (
-    <div className="Wrapper">
-      <h1 className="Title">{title}</h1>
-      <form onSubmit={changeTitle}>
-        <div className="Input">
-          <input
-            className="Input-text"
-            id="input"
-            name="inputText"
-            onChange={handleChanges}
-            placeholder="Create new title"
-            type="text"
-            value={inputText}
-          />
-          <label htmlFor="input" className="Input-label">
-            New title
-          </label>
-        </div>
-      </form>
+ return (
+  <div className="Wrapper">
+   <h1 className="Title">{title}</h1>
+   <form onSubmit={changeTitle}>
+    <div className="Input">
+     <input
+      className="Input-text"
+      id="input"
+      name="inputText"
+      onChange={handleChanges}
+      placeholder="Create new title"
+      type="text"
+      value={inputText}
+     />
+     <label htmlFor="input" className="Input-label">
+      New title
+     </label>
     </div>
-  );
+   </form>
+  </div>
+ );
 };
 
 export default DynamicTitle;
