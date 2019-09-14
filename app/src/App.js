149db2ddb3b47ccc06df547f45f304a6778b6b3d
Dil Sombayeva <dilyasom@gmail.com>
2019-09-14 08:28:07 -0700

    localStorage use custom hook

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,5 @@
 import React, { useState } from 'react';
-// import DynamicTitle from './DynamicTitle';
 import './App.css';
-
-// const useInput = (key, initialValue) => {
-//  const [inputValue, setInputValue] = useLocalStorage(key, initialValue);
-
-//  const handleChange = updatedValue => {
-//   setInputValue(updatedValue);
-//  };
-
-//  return [inputValue, handleChange];
-// };
 
 // ************************************************
 const useLocalStorage = (key, val) => {
@@ -32,12 +21,10 @@ function App() {
  const [password, setPassword] = useState('');
  const [isValid, setIsValid] = useState(false);
 
- // ************************************************
  const [usernameTwo, setUsernameTwo] = useLocalStorage('usernameTwo', '');
  const [passwordTwo, setPasswordTwo] = useLocalStorage('passwordTwo', '');
  // ************************************************
 
- //  const passValidatorArray = usePasswordValidator();
  const validatePass = pass => {
   if (pass.length > 7) {
    setIsValid(true);
@@ -45,11 +32,9 @@ function App() {
    setIsValid(false);
   }
  };
- //  console.log(passValidatorArray);
- console.log(isValid, setIsValid);
+
  return (
   <div className="App">
-   {/* <DynamicTitle /> */}
    <p>Make sure your password is at least 8 chars</p>
    <form action="">
     <label>Create a New Password</label>
